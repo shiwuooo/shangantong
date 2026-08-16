52eeb31fab8bde14b9861f5c46fae208c42edf5a
shangantong-deploy <deploy@local>
2026-08-16 19:48:27 +0800

    还原模考库必需文件(mock-manifest + fenbi-mock + 15个fb-mock), 撤销误删

---
+++ mock-manifest.js
@@ -0,0 +1,18 @@
+window.MOCK_BANK_FILES = [
+  "fenbi-mock-structure.js",
+  "fb-mock-2bca0ee7.js",
+  "fb-mock-423d10cd.js",
+  "fb-mock-5cccb959.js",
+  "fb-mock-67cbda1f.js",
+  "fb-mock-6c98c709.js",
+  "fb-mock-9ce602be.js",
+  "fb-mock-a2b430de.js",
+  "fb-mock-a4f92eaa.js",
+  "fb-mock-ab632f09.js",
+  "fb-mock-b4c79e32.js",
+  "fb-mock-d8954632.js",
+  "fb-mock-e0c860a3.js",
+  "fb-mock-e2048e75.js",
+  "fb-mock-efb401d2.js",
+  "fb-mock-f228e0eb.js"
+];
